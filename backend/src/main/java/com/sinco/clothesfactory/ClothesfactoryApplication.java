790e2a35ecc7440a3fe47b05635ed7e37dde81b3
Edinson Villalba <edvillalba@outlook.com>
2018-11-29 11:57:49 -0500

    Initial Commit

---
+++ backend/src/main/java/com/sinco/clothesfactory/ClothesfactoryApplication.java
@@ -0,0 +1,12 @@
+package com.sinco.clothesfactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothesfactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClothesfactoryApplication.class, args);
+	}
+}
---
+++ backend/src/main/java/com/sinco/clothesfactory/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.sinco.clothesfactory.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/test")
+public class TestController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String findAll() {
+        return "Test";
+    }
+
+}
